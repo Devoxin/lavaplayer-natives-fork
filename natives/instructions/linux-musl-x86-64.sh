c82fda899807e3dc5b30fb733083838b2d985b1c
Devoxin <lukel9914@gmail.com>
2024-05-25 18:23:17 +0100

    instructions for musl x86-64

--- natives/instructions/linux-musl-x86-64.sh
+++ natives/instructions/linux-musl-x86-64.sh
@@ -1,18 +1,13 @@
 docker run --rm -it alpine sh
 
 apk update && apk upgrade
-apk add git cmake build-base autoconf openjdk8 wget
+apk add git cmake build-base autoconf openjdk11 wget libtool
 
-wget https://mirror2.sandyriver.net/pub/software/gnu/automake/automake-1.15.1.tar.gz
-tar -zxvf automake-1.15.1.tar.gz
-cd automake-1.15.1
-./configure
-make install
-
-git clone https://github.com/Walkyst/lavaplayer-natives-fork
+git clone https://github.com/devoxin/lavaplayer-natives-fork
 cd lavaplayer-natives-fork
+
+# DOWNGRADE libmpg123 TO 1.31.3
+
 chmod +x ./gradlew
-cd natives
-../gradlew load
-touch opus/opus-1.3/aclocal.m4 configure
-../gradlew compileNatives
+./gradlew load
+./gradlew compileNatives
